4726c32afb699ab058e2bd6e41554da11a6af83d
TeghveerK <tkooner@uci.edu>
2023-01-02 15:07:49 -0800

    Card and Button Component

--- src/Components/Users/AddUser.js
+++ src/Components/Users/AddUser.js
@@ -1,21 +1,29 @@
 import React from "react";
 import styles from "./AddUser.module.css";
+import Card from "../UI/Card";
+import Button from "../UI/Button";
 
 const AddUser = (props) => {
   const addUserHandler = (event) => {
     event.preventDefault();
   };
 
+  const onClickHandler = (event) => {
+    event.preventDefault();
+  };
+
   return (
-    <form onSubmit={addUserHandler}>
-      <div className={`${styles["input"]}`}>
+    <Card className={styles.input}>
+      <form onSubmit={addUserHandler}>
         <label htmlFor="username">Username</label>
         <input id="username" type="text" />
         <label htmlFor="age">Age (Years)</label>
         <input id="age" type="number" />
-        <button type="submit">Add User</button>
-      </div>
-    </form>
+        <Button type="submit" onClick={onClickHandler}>
+          Add User
+        </Button>
+      </form>
+    </Card>
   );
 };
 
